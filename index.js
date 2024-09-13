e7f55438b5ad8f011344229b26ffc2769f4a0cd1
Luiz <luizfernando112034@gmail.com>
2024-09-13 09:33:27 -0300

    fundamentos 002

---
+++ index.js
@@ -0,0 +1,17 @@
+// arrays, objetos
+let meta = {
+    value: 'ler um livro por mês',
+    address:2,
+    checked: true,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.value = "não é mais ler um livro"
+meta.log(meta.value)
+
+// funcion // arrow function
+//const criarMeta = () => {}
+
+//function criarMeta() {}
