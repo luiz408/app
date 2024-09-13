bb963f8c7610a5abc0b32eec8e395a69c088bb44
Luiz <luizfernando112034@gmail.com>
2024-09-13 09:51:54 -0300

    fundamnetos 004

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-let meta = {
-    value: 'ler um livro por mês',
-    checked: true,
+const start = () => {
+    let count = 0
+    
+    while(count <= 10){
+        console.log(count)
+        count = count + 1
+    }
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhas 20 minutos todos os dias",
-        checked: false
-    }
-]
-
-console.log(metas[1].value)
+start()
